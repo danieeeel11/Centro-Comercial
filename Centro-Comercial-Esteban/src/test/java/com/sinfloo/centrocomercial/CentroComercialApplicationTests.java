f89b8aec57a2e58e59564023c5413b5a2d4320e8
danieeeel11 <111476105+danieeeel11@users.noreply.github.com>
2023-03-29 15:41:15 -0500

    Add files via upload

---
+++ Centro-Comercial-Esteban/src/test/java/com/sinfloo/centrocomercial/CentroComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinfloo.centrocomercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroComercialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
