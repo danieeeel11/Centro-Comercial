f8b25b50ca18826d7a142154276721d8622a0fac
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-04-04 01:47:23 +0000

    A

--- src/main/java/com/sinfloo/centrocomercial/controller/Controlador.java
+++ src/main/java/com/sinfloo/centrocomercial/controller/Controlador.java
@@ -8,15 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/cc")
