053089ac2487f82091f99d14db8d6f60994c89b4
David Arbeláez <davidsarbelaez@gmail.com>
2023-04-05 23:48:18 -0500

    Agrego carrusel semi funcionando

--- src/main/resources/static/Index/index.js
+++ src/main/resources/static/Index/index.js
@@ -1,6 +1,6 @@
 var estado = 0;
 const star = document.querySelector(".btn.star_cc");
-function addFav2() {
+function addFav() {
     if (estado == 0) {
         star.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Estrella_amarilla.png/2048px-Estrella_amarilla.png')";
         estado = 1;
