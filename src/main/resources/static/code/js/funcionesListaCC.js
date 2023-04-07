900b53a496557e007909d520bac1832ef746c44f
danieeeel11 <danielfvr01@gmail.com>
2023-04-07 14:08:35 -0500

    Mejoras carrusel, listaCC

--- src/main/resources/static/code/js/funcionesListaCC.js
+++ src/main/resources/static/code/js/funcionesListaCC.js
@@ -69,18 +69,22 @@ function paintCC(data){
         }
         if(fila == 0){
             code += `
-                <a href="../lista/infoCC.html" onclick="setId(${data[i].id})" class="btn cc" style="left:${des_lat}%;background-image:url('${data[i].logo}')">
-                    <p class='txt_btn_cc'> ${data[i].nombre} </p>
+                <div class="sec_btn_cc" style="left:${des_lat}%">
+                    <a href="../lista/infoCC.html" onclick="setId(${data[i].id})" class="btn cc" style="background-image:url('${data[i].logo}')">
+                        <p class='txt_btn_cc'> ${data[i].nombre} </p>
+                    </a>
                     <button id="${data[i].nombre}" class="btn star_cc" onclick="addFav('${data[i].nombre}')"></button>
-                </a>`;
+                </div>`;
             conteo++;
             des_lat += 23;
         }else if(fila==1) {
             code += `
-                <a href="../lista/infoCC.html" onclick="setId(${data[i].id})" class="btn cc" style="left:${des_lat}%;top:${des_top}%;background-image:url('${data[i].logo}')">
-                    <p class='txt_btn_cc'> ${data[i].nombre} </p>
+                <div class="sec_btn_cc" style="left:${des_lat}%;top:${des_top}%;">
+                    <a href="../lista/infoCC.html" onclick="setId(${data[i].id})" class="btn cc" style="background-image:url('${data[i].logo}')">
+                        <p class='txt_btn_cc'> ${data[i].nombre} </p>
+                    </a>
                     <button id="${data[i].nombre}" class="btn star_cc" onclick="addFav('${data[i].nombre}')"></button>
-                </a>`;
+                </div>`;
             conteo++;
             des_lat += 23;
         }
@@ -90,19 +94,4 @@ function paintCC(data){
 
 function setId(id){
     window.localStorage.setItem('id',id);
-}
-
-var estado = 0;
-function addFav(name){
-    console.log(name);
-    let sec = document.getElementById(name);
-    //console.log(sec);
-    //const star = sec.querySelector("btn.star_cc");
-    if(estado==0){
-        sec.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Estrella_amarilla.png/2048px-Estrella_amarilla.png')";
-        estado = 1;
-    }else{
-        sec.style.backgroundImage = "url('https://cdn-icons-png.flaticon.com/512/16/16666.png')";
-        estado = 0;
-    }
 }
