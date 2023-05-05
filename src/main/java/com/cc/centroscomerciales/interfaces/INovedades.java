e2a2c4a277b497b9b95526f93b67162a66bf7eaa
DavidSArbelaez <davidsarbelaez@gmail.com>
2023-05-05 10:15:16 -0500

    Agrego cambios pequeños,se borraron @de distintas clases de java

--- src/main/java/com/cc/centroscomerciales/interfaces/INovedades.java
+++ src/main/java/com/cc/centroscomerciales/interfaces/INovedades.java
@@ -2,8 +2,6 @@ package com.cc.centroscomerciales.interfaces;
 
 import com.cc.centroscomerciales.modelo.Novedades;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface INovedades extends CrudRepository<Novedades, Integer> {
 }
--- src/main/java/com/cc/centroscomerciales/interfaces/ITiendas.java
+++ src/main/java/com/cc/centroscomerciales/interfaces/ITiendas.java
@@ -2,9 +2,5 @@ package com.cc.centroscomerciales.interfaces;
 
 import com.cc.centroscomerciales.modelo.Tiendas;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-//Clase que se conecta con la Base de Datos
 public interface ITiendas extends CrudRepository<Tiendas, Integer> {
 }
